f5e55fbb8f70d00dd37f677177c93d9f19fc4f13
Duncan Moyo <duncan@darkpools.co.za>
2022-12-24 14:28:54 +0200

    first commit

---
+++ client/src/components/DataGridCustomColumnMenu.jsx
@@ -0,0 +1,13 @@
+import { GridColumnMenuContainer, GridFilterMenuItem, HideGridColMenuItem } from "@mui/x-data-grid";
+
+const DataGridCustomColumnmenu = (props ) => {
+  const {hideMenu, currentColumn, open} = props;
+    return (
+    <GridColumnMenuContainer hideMenu={hideMenu} currentColumn={currentColumn} open={open} >
+      <GridFilterMenuItem onClick={hideMenu} column={currentColumn} />
+      <HideGridColMenuItem onClick={hideMenu} column={currentColumn} />
+    </GridColumnMenuContainer>
+  )
+}
+
+export default DataGridCustomColumnmenu
